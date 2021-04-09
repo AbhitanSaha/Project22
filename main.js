778d84a44987a8ca5bd8f533c31195930f17d3f7
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2021-04-09 21:12:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 function setup(){
    canvas= createCanvas(500,400);
    canvas.position(700,75);
+   video= createCapture(VIDEO);
+   video.size(400,500);
+   poseNet=ml5.poseNet(video,gotResults);
+   poseNet.on("pose",gotPoses);
+}
+function draw(){
+   background('#4f0f30');
+}
+function gotResults(){
+   console.log("it worked my dumb science");
+}
+function gotPoses(results){
+   if(results.length>0){
+      console.log(results);
+   }
 }
