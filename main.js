cd973189c4e8c727eb46f6d0bee20704dbd0f403
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2021-04-13 21:11:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
+diffrence=0;
+rightWristX=0;
+leftWristX=0;
 function setup(){
    canvas= createCanvas(500,400);
-   canvas.position(700,75);
+   canvas.position(700,100);
    video= createCapture(VIDEO);
    video.size(400,500);
    poseNet=ml5.poseNet(video,gotResults);
    poseNet.on("pose",gotPoses);
 }
 function draw(){
+   document.getElementById("filler").innerHTML="Width an Height are="+diffrence+"px";
    background('#4f0f30');
+   fill("#FFFFFF");
+   textSize(diffrence);
+   text("Peter",200,200);
 }
 function gotResults(){
    console.log("it worked my dumb science");
@@ -15,5 +22,9 @@ function gotResults(){
 function gotPoses(results){
    if(results.length>0){
       console.log(results);
+      rightWristX=results[0].pose.rightWrist.x;
+      leftWristX=results[0].pose.leftWrist.x;
+      diffrence= floor(rightWristX-leftWristX);
+      console.log("rightWristX="+rightWristX+"leftWristX="+leftWristX+"diffrence="+diffrence);
    }
 }
